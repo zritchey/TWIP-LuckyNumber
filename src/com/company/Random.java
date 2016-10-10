a8c2efa712f3327869a7acbce1ae60353f2291bf
zritchey <zachrritchey@gmail.com>
2016-10-10 10:34:17 -0400

    Initial commit

---
+++ src/com/company/Random.java
@@ -0,0 +1,37 @@
+package com.company;
+
+/**
+ * Created by zr162 on 10/10/16.
+ */
+import java.util.*;
+public class Random {
+
+    public String name;
+    public boolean sq;
+    public double randSQ;
+    public double root;
+ public Random con=new Random;
+
+    public Random(String name)
+    {
+        this.name=name;
+        sq=false;
+
+
+    }
+
+    public double getRandSQ()
+    {
+        randSQ=
+    }
+
+    private boolean pSquare(double i){
+        if (i==(int)i)
+            sq=true;
+        else
+            sq=false;
+        return sq;
+    }
+
+
+}
